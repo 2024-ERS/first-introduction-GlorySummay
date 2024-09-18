0f9e5b31f2b5850953177ea0cff409113a275ef3
Glory Summay <g.summay@rug.nl>
2024-09-18 08:34:00 +0200

    changes

--- r-scripts/seabirds.R
+++ r-scripts/seabirds.R
@@ -8,33 +8,32 @@ rm(list=ls())
 renv::restore()
 library(tidyverse) 
 # load the elevation data and show the first 10 records of the dataset
-elevdat<-read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vT4C7olgh28MHskOjCIQGYlY8v5z1sxza9XaCccwITnjoafF_1Ntfyl1g7ngQt4slnQlseWT6Fk3naB/pub?gid=1550309563&single=true&output=csv") |>
-  dplyr::mutate(year=factor(year)) # make year a factor
+elevdat<-readr::read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vT4C7olgh28MHskOjCIQGYlY8v5z1sxza9XaCccwITnjoafF_1Ntfyl1g7ngQt4slnQlseWT6Fk3naB/pub?gid=1550309563&single=true&output=csv") %>%
+  dplyr::filter(year %in% c(2019,2021,2022,2023,2024) & !is.na(TransectPoint_ID))|>
+  dplyr::select(year,TransectPoint_ID,elevation_m)   # select  only distance_m and elevation 
 elevdat
-
 # plot the change in transect  elevation along the transect, using a separate graph for each for each year 
 elevdat |>
   ggplot(aes(x=TransectPoint_ID, y=elevation_m, col=year)) +geom_line()
 
 # plot the change in transect  elevation along the transect, using a separate line color for each year 
 
-# Extract the data for 2017 in a new tibble, keep only variables distance_m and elevation
-# omit records where Distance_ID is missing (NA)
-elevdat2017 <- elevdat |>
-  dplyr::filter(year=="2017") |>
-  dplyr::select(TransectPoint_ID,elevation_m) |>
-  dplyr::filter(!is.na(TransectPoint_ID))
- 
-# read the cockle data 
-# keep only the data for 2017, 
-# omit observations (Obs_ID) 468 and 1531
-# calculate the mean number of cockles and mean size for each distance
-cdat2017 <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSpormjGiM4uWMTIZYm9Upt5j1Ige_Wu9CrhGGdvjXwxmHP2R6S7JDyEmiGTn3fXihl5s5yBn_sdo8h/pub?gid=1538766002&single=true&output=csv") |>
-  
+# read the birds abundance data, call the dataset birds
+# filter to use only years 2019,2021,2022,2023,2024 and only 3 or less replicates and species Orchestia_gammarellus
+# group by year and TransectPoint_ID
+# calculate the sum of the number of birds found per year and TransectPoint
+# do all the above in one pipeline
+# calculate the mean number of birds and mean size for each distance
+birdsdat<-read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRRwj4K6riKOkORXZ7NcrjocSmHtpY2ecxpBSRXasjfqy3ySJpiaps5evSa90Tk_m5IEmDMk8n1qSlg/pub?gid=822377591&single=true&output=csv") |>
+  dplyr::filter(year %in% c(2019,2021,2022,2023,2024))|>
+  dplyr::group_by(Year,Species) |>
+  dplyr::summarise(CountSum=sum(Count, na.rm=TRUE))
+print(birdsdat)
+
 dplyr::filter(year==2017,
-              CockleObs_ID!=468,
-              CockleObs_ID!=1531) |>
-dplyr::group_by(TransectPoint_ID) |> #group by distance
+                CockleObs_ID!=468,
+                CockleObs_ID!=1531) |>
+  dplyr::group_by(TransectPoint_ID) |> #group by distance
   dplyr::summarize(n_obs=n(),
                    avg_l=mean(length_mm, na.rm=T),
                    sd_l=sd(length_mm,na.rm=T),
@@ -50,16 +49,16 @@ print(cdat2017)
 # using Distance_ID as the common variable between the two tables
 
 combidat<-dplyr::left_join(elevdat2017,cdat2017,by="TransectPoint_ID") |>
-dplyr::mutate(n_obs=tidyr::replace_na(n_obs,0))
+  dplyr::mutate(n_obs=tidyr::replace_na(n_obs,0))
 combidat
 
 
 # show in a plot how cockle density changes with elevation
-  combidat |>
+combidat |>
   ggplot(aes(x=elevation_m, y=n_obs)) +
   geom_point() + 
   # fit a linear regression
-geom_smooth(method="loess")
+  geom_smooth(method="loess")
 
 
 # predicted at 0.5 m (x)
